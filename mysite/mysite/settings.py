0aae1cc8c6562c5913cee08d1dc49b3f22921d20
Trung <you@example.com>
2022-04-29 13:44:29 +0700

    skfjsf

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -137,7 +137,7 @@ DATABASES['default'].update(db_from_env)
 STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
 
 # The URL to use when referring to static files (where they will be served from)
-STATIC_URL = '/polls/static/polls/'
+STATIC_URL = '/polls/static/'
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
